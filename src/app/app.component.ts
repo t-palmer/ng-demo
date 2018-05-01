00b977b8c5d34a75afcf7960bb643391644ae471
Todd Palmer <tpalmer@us.fujitsu.com>
2018-04-30 22:25:28 -0400

    Remove package-lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-      <h1>ng-demo</h1>
-      <p>This project provides some examples of coding  in Angular.
-          Specifically, it deals with:</p>
-      <ul>
-          <li><a routerLink="/binding">Bootstrap and ng-bootstrap</a></li>
-          <li><a routerLink="/binding">Displaying Code in Angular</a></li>
-          <li><a routerLink="/binding">Binding</a></li>
-          <li><a routerLink="/this">this</a></li>
-          <li><a routerLink="/observables">Observables</a></li>
-      </ul>
-      <router-outlet></router-outlet>
+      <div class="container">
+        <h1>ng-demo</h1>
+        <p>This project provides some examples of coding  in Angular.
+            Specifically, it deals with:</p>
+        <ul>
+            <li><a routerLink="/binding">Bootstrap and ng-bootstrap</a></li>
+            <li><a routerLink="/binding">Displaying Code in Angular</a></li>
+            <li><a routerLink="/binding">Binding</a></li>
+            <li><a routerLink="/this">this</a></li>
+            <li><a routerLink="/observables">Observables</a></li>
+        </ul>
+        <router-outlet></router-outlet>
+      </div>
   `,
   styles: []
 })
